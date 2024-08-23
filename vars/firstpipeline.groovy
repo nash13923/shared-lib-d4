9edd7b4fe03103bedf8dce71cded5db8822209de
nash13923 <nash13923@gmail.com>
2024-08-23 14:23:43 +0530

    added shared code

---
+++ vars/firstpipeline.groovy
@@ -0,0 +1,31 @@
+// Pipeline + groovy 
+// import the Calcclulator class from com.i27academy.build package
+import com.i27academy.builds.Calculator
+
+def call(Map pipelineparams){
+    // An instance of the class called calculator is created
+    Calculator calculator = new Calculator(this)
+
+    pipeline {
+        agent any 
+        environment {
+            APP_NAME = "${pipelineparams.appName}" // this value should be coming from microservices 
+        }
+        stages {
+            stage('AdditionStage') {
+                steps {
+                    script {
+                    echo "Printing Sum of 2 numbers"
+                    println calculator.add(3,4) // add expects 2 parameters
+                    echo "****** Microservice Name is: ${APP_NAME} ********"
+                    }
+                }
+            }
+            stage ('SecondStage') {
+                steps {
+                    echo "Printing second stage"
+                }
+            }
+        }
+    }
+}
